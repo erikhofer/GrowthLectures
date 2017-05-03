a1f34a88eee28f61adaee55685bbf72ccc17d9a8
Erik Hofer <erik-hofer@mail.de>
2017-05-03 14:23:44 +0200

    Created Gradle project using Spring initializr.

---
+++ src/main/java/com/xinra/GrowthlecturesApplication.java
@@ -0,0 +1,12 @@
+package com.xinra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthlecturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthlecturesApplication.class, args);
+	}
+}
