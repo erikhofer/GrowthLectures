ed55336c84ea97e4bc4b90f26d214e4463c681d4
Lorenz Kock <l@lkock.de>
2017-05-23 20:47:50 +0200

    Added Login Panel. #7

---
+++ src/main/groovy/com/xinra/growthlectures/frontend/LoginController.java
@@ -0,0 +1,22 @@
+package com.xinra.growthlectures.frontend;
+
+import com.xinra.nucleus.service.DtoFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+   
+  @Autowired
+  private DtoFactory dtoFactory;
+  
+  @RequestMapping("/login")
+  public String index(Model model) {
+       
+    return "login";
+  }
+
+}
