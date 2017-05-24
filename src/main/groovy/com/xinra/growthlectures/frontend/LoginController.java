39d4b746e5be27b22a33f7a7310d58a1c6a81931
Lorenz Kock <l@lkock.de>
2017-05-24 11:00:58 +0200

    Added Login object to form. Added ErrorMessage-Alert. #7

--- src/main/groovy/com/xinra/growthlectures/frontend/LoginController.java
+++ src/main/groovy/com/xinra/growthlectures/frontend/LoginController.java
@@ -1,10 +1,12 @@
 package com.xinra.growthlectures.frontend;
 
+import com.xinra.growthlectures.service.EmailLoginDto;
 import com.xinra.nucleus.service.DtoFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -14,8 +16,22 @@ public class LoginController {
   private DtoFactory dtoFactory;
   
   @RequestMapping("/login")
-  public String index(Model model) {
+  public String loginPage(Model model) {
+    
+    model.addAttribute("loginUser", dtoFactory.createDto(EmailLoginDto.class));
+    
+    return "login";
+  }
+  
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  public String loginPagePost(Model model, EmailLoginDto user) {
        
+    System.out.println(user.getEmail());
+    System.out.println(user.getPassword());
+    
+    model.addAttribute("loginUser", user);
+    model.addAttribute("errorMessage", "Incorrect username or password!");
+    
     return "login";
   }
 
