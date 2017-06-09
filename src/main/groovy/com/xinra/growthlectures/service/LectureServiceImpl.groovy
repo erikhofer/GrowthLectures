62c6f26153013627bfdf0e7a43ee59a6df2fbf10
Lorenz Kock <l@lkock.de>
2017-06-09 09:25:59 +0200

    Add converter from Lecture to Dto. #15

--- src/main/groovy/com/xinra/growthlectures/service/LectureServiceImpl.groovy
+++ src/main/groovy/com/xinra/growthlectures/service/LectureServiceImpl.groovy
@@ -1,5 +1,7 @@
 package com.xinra.growthlectures.service
 
+import com.xinra.growthlectures.entity.Lecture
+import com.xinra.growthlectures.entity.LectureRepository
 import com.xinra.nucleus.service.DtoFactory
 import groovy.transform.CompileStatic
 import java.time.LocalDate
@@ -13,12 +15,15 @@ class LectureServiceImpl extends GrowthlecturesServiceImpl implements LectureSer
 	@Autowired
 	private DtoFactory dtoFactory;
 	
+  @Autowired
+  private LectureRepository lecureRepo;
+  
 	//TODO: remove
 	private LectureSummaryDto getSampleLecture() {
 		LectureSummaryDto dto = dtoFactory.createDto(LectureSummaryDto.class);
 		NamedDto cat = new NamedDtoImpl() as NamedDto;
-		cat.setName("Peter");
-		cat.setSlug("peter");
+		cat.setName("Fernsehsendung");
+		cat.setSlug("fernsehsendung");
 		dto.setCategory(cat);
 		dto.description = "Das ist die Beschreibung";
 		dto.duration = 783;
@@ -33,6 +38,26 @@ class LectureServiceImpl extends GrowthlecturesServiceImpl implements LectureSer
     
 		return dto as LectureSummaryDto;
 	}
+  
+  private LectureSummaryDto convertToDto(Lecture lecture) {
+    LectureSummaryDto dto = dtoFactory.createDto(LectureSummaryDto.class);
+    dto.description = lecture.getDescription();
+    dto.rating = lecture.getRatingAverage()
+    NamedDto cat = new NamedDtoImpl() as NamedDto;
+    cat.setName(lecture.getCategory().getName());
+    cat.setSlug(lecture.getCategory().getSlug());
+    dto.setCategory(cat);
+    dto.name = lecture.getName();
+    dto.slug = lecture.getSlug();
+    NamedDto lecturer = dtoFactory.createDto(NamedDto.class);
+    lecturer.setName(lecture.getLecturer().getName());
+    lecturer.setSlug(lecture.getLecturer().getSlug());
+    dto.setLecturer(lecturer);
+    dto.setAdded(lecture.getAdded());
+    
+    return dto;
+  }
+  
 
 	public List<LectureSummaryDto> getRecentLectures() {
 		List<LectureSummaryDto> dtos = new ArrayList<>();
@@ -49,5 +74,7 @@ class LectureServiceImpl extends GrowthlecturesServiceImpl implements LectureSer
 		}
 		return dtos;
   }
+  
+  
 	
 }
