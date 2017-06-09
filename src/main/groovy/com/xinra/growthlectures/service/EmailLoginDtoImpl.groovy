c32b0ec5ab7bd826d8f2e9c7ada3dd787245fe00
Erik Hofer <erik-hofer@mail.de>
2017-06-09 10:17:42 +0200

    Removed bean validation (currently not working with Groovy properties). #5

--- src/main/groovy/com/xinra/growthlectures/service/EmailLoginDtoImpl.groovy
+++ src/main/groovy/com/xinra/growthlectures/service/EmailLoginDtoImpl.groovy
@@ -1,8 +1,6 @@
 package com.xinra.growthlectures.service
 
-import com.xinra.growthlectures.validation.ValidEmail
 import groovy.transform.CompileStatic
-import javax.validation.constraints.NotNull
 
 @CompileStatic
 class EmailLoginDtoImpl extends GrowthlecturesDtoImpl {
