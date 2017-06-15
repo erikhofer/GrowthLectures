e50d90aae8432fcc1ca48b52fc9452323bbb1a45
Lorenz Kock <l@lkock.de>
2017-06-15 16:11:24 +0200

    Add authentication check when adding a category / lecture.

--- src/main/groovy/com/xinra/growthlectures/frontend/SecurityConfiguration.java
+++ src/main/groovy/com/xinra/growthlectures/frontend/SecurityConfiguration.java
@@ -1,12 +1,14 @@
 package com.xinra.growthlectures.frontend;
 
 import com.xinra.growthlectures.entity.EmailLoginRepository;
+import com.xinra.growthlectures.entity.Role;
 import com.xinra.growthlectures.service.AuthenticationProviderImpl;
 import com.xinra.growthlectures.service.EmailLoginDto;
 import com.xinra.growthlectures.service.UserDetailsServiceImpl;
 import com.xinra.nucleus.service.DtoFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,6 +43,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
             .antMatchers("/").permitAll()
             .antMatchers(Ui.URL_CATEGORIES + "/**/note").authenticated()
+            .antMatchers(HttpMethod.POST, Ui.URL_CATEGORIES)
+                .hasAuthority(Role.MODERATOR.toString())
+            .antMatchers(HttpMethod.POST, Ui.URL_LECTURERS)
+                .hasAuthority(Role.MODERATOR.toString())
         .and()
             .formLogin()
             .loginPage(Ui.URL_LOGIN)
