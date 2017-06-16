8b359b26d6e8dd3abf9c0061745d0880be8f083f
Lorenz Kock <l@lkock.de>
2017-06-16 08:39:19 +0200

    Show error message when no videos can be found. #40

--- src/main/groovy/com/xinra/growthlectures/GrowthlecturesApplication.java
+++ src/main/groovy/com/xinra/growthlectures/GrowthlecturesApplication.java
@@ -96,9 +96,9 @@ public class GrowthlecturesApplication implements CommandLineRunner {
     Lecture lecture2 = new Lecture();
     lecture2.setAdded(LocalDate.now());
     lecture2.setDescription("How it is (wap bap) AVAILABLE EVERYWHERE NOW: http://wmg.click/BibiH_HowItIsAY * STORES Germany: - Amazon - Media Markt - Saturn - Müller - Expert - Medimax - Weltbild - jpc.de Austria: - Media Markt - Müller DOWNLOAD: - ITunes - Amazon - GooglePlay STREAM: - Spotify - AppleMusic - Deezer - Napster - Google Music by: Sam Sommer Lyrics by: Sam Sommer | Dave Knight Produced by: Chinzilla Films Special thanks to Warner Music Germany -- | LYRICS | -- Verse 1 It’s late at night I go to bed But I can’t get no rest My boyfriend quit I’m almost dead I’ll have to do my best! I’m up and down I feel so fat I ain’t got no more fizz Don’t even get to keep the cat But that’s just how it is! Chorus 1 I Sing… Wap bap wah da de da dah Dap bap bah da de da dah Dap bap bah da de da dah dah! Wap bap wah da de da dah Dap bap wah da de da dah Dap bap Bah da de da dah dah! Verse 2 I get up late I stub my toe I’ve got a few missed calls I lost my job I didn’t know And that’s not good at all! I get my bag I go outside Just minding my own biz I go to pay but my card has been denied But that’s just how it is! Chorus 2 I Sing… Wap bap wah da de da dah Dap bap bah da de da dah Dap bap bah da de da dah dah! And everybody sing… Wap bap wah da de da dah Dap bap wah da de da dah Dap bap Bah da de da dah dah Verse 3 Another week the same old shit My troubles getting worse And then I get a bigger hit Cos someone stole my purse! I met a guy he knows my name He also told me his And if we want to have some fun Well that’s just how it is Chorus 3 * Product Placement - Special thanks to Warner Music Germany");
-    lecture2.setCategory(c2);
+    lecture2.setCategory(c);
     lecture2.setName("Bibi H - How it is ( wap bap ... ) [Official Video]");
-    lecture2.setLecturer(l2);
+    lecture2.setLecturer(l);
     lecture2.setRatingAmount(3);
     lecture2.setRatingAverage(1.5);
     lecture2.setSlug("bibi-h-how-it-is-wap-bap-official-video");
